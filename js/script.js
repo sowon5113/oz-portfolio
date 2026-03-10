b356006a78e8fd4487e112b7506e8b210a604adf
sowon <sowon5113@gmail.com>
2026-03-10 15:31:47 +0900

    Ver.1.0.0

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// /* stylelint-disable @stylistic/selector-list-comma-newline-after */
+
+// .blog-header-logo {
+//   font-family: Amiri, Georgia, "Times New Roman", serif;
+//   font-size: 2.25rem;
+// }
+
+// .blog-header-logo:hover {
+//   text-decoration: none;
+// }
+
+// h1, h2, h3, h4, h5, h6 {
+//   font-family: Amiri, Georgia, "Times New Roman", serif;
+// }
+
+// .flex-auto {
+//   flex: 0 0 auto;
+// }
+
+// .h-250 { height: 250px; }
+// @media (min-width: 768px) {
+//   .h-md-250 { height: 250px; }
+// }
+
+// /* Pagination */
+// .blog-pagination {
+//   margin-bottom: 4rem;
+// }
+
+// /*
+//  * Blog posts
+//  */
+// .blog-post {
+//   margin-bottom: 4rem;
+// }
+// .blog-post-meta {
+//   margin-bottom: 1.25rem;
+//   color: #727272;
+// }
+
+// 1. AOS 라이브러리 초기화
+AOS.init({
+    duration: 1000, // 애니메이션 지속 시간 (1초)
+    once: false,     // 스크롤 올릴 때 다시 반복할지 여부
+});
+
+// 2. 추가적인 커스텀 기능 (예: 클릭 이벤트 등)
+console.log("자바스크립트 파일이 성공적으로 연결되었습니다!");
